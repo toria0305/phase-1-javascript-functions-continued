309345d534d01135072f464237d2e731f936f08d
Victoria Maggay <toria0305@gmail.com>
2022-11-01 13:13:28 -0400

    commit tree 692c75518177165bfa884f102bb71199c52eda1f

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function saturdayFun(defaultactivity = 'roller-skate') {
+    return `This Saturday, I want to ${defaultactivity}!`  
+}
+
+function mondayWork(defAct = 'go to the office') {
+    return `This Monday, I will ${defAct}.`
+}
+
+let wrapAdjective = function(special = '*') {
+    return function(adjective= 'special') {
+    return `You are ${special}${adjective}${special}!`
+}
+}
